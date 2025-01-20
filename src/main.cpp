386bf6a5ec9d52e85d4c9ea1eb36cee73f2804ff
Calico Randall <CRandall@omsi.edu>
2025-01-20 12:13:36 -0800

    Working (with teensy3.2)
    Code working with Teensy 3.2

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,41 @@
 // January 20, 2025
 
 #include <Arduino.h>
+#include "Adafruit_VL53L0X.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_VL53L0X TOFS = Adafruit_VL53L0X();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  while(!Serial) {
+    delay(1);
+  }
+
+  Serial.println("Adafruit VL53L0X test.");
+  if (!TOFS.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+
+  Serial.println("Time of Flight sensor booted.");
+  // if(!TOFS.begin()) {
+  //   Serial.println(F("Failed to boot Time of Flight sensor."));
+  //   while(1);
+  // }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  TOFS.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println("Out of range");
+  }
+    
+  delay(100);
 }
